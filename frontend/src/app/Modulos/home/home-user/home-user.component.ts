77dffdf65bf22f68adf3ca82213c571c00807b38
Christian Sánchez Millán <chris.sanchez.millan@gmail.com>
2024-11-18 19:31:58 +0100

    creación de la carpeta home-user dentro de home en la branch chris/home-user

---
+++ frontend/src/app/Modulos/home/home-user/home-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-user.component.html',
+  styleUrl: './home-user.component.css'
+})
+export class HomeUserComponent {
+
+}
