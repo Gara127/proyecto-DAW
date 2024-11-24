f92b46381c17c78378f86a010f1776b8930af738
Christian Sánchez Millán <chris.sanchez.millan@gmail.com>
2024-11-24 13:46:19 +0100

    añadido el modulo home-user y la ruta dentro de home-routing.module.ts y home.module.ts

--- frontend/src/app/Modulos/home/home-routing.module.ts
+++ frontend/src/app/Modulos/home/home-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EventCreatorComponent } from './event-creator/event-creator.component';
+import { HomeUserComponent } from './home-user/home-user.component'; // Importa el componente principal
 
 const routes: Routes = [
-  { path: 'event-creator', component: EventCreatorComponent}
+  { path: '', component: HomeUserComponent }, // Ruta principal del módulo Home
+  { path: 'event-creator', component: EventCreatorComponent } // Añadida la ruta delboton crear evento de la HOME
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class HomeRoutingModule { }
+export class HomeRoutingModule {}
+
