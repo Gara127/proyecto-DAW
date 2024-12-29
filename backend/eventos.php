0770fc5235d9175018bbd99f06a40cf78f6086b5
Gara <gara127gs@gmail.com>
2024-12-29 14:16:51 +0000

    Specified FE url header

--- backend/eventos.php
+++ backend/eventos.php
@@ -1,6 +1,7 @@
 <?php
 header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://crewconnect.netlify.app");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
--- backend/usuarios.php
+++ backend/usuarios.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
 header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://crewconnect.netlify.app");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
